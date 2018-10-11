c1b556471ff7f980b2aebd112e951513a16ba8c3
rahul kumar <rahulkumar11501@gmail.com>
2018-10-11 06:15:15 -0700

    WIP devise-jwt

---
+++ devise-jwt/db/migrate/20181011125016_create_jwt_blacklist.rb
@@ -0,0 +1,9 @@
+class CreateJwtBlacklist < ActiveRecord::Migration[5.0]
+  def change
+    create_table :jwt_blacklists do |t|
+  	t.string :jti, null: false
+    end
+
+    add_index :jwt_blacklist, :jti
+  end
+end
