11a020ced28283421014fd7910d522cb20d0fa92
rahul kumar <rahulkumar11501@gmail.com>
2018-10-14 10:36:43 -0700

    WIP session

--- thinkster_jwt_tut/app/controllers/application_controller.rb
+++ thinkster_jwt_tut/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
--- thinkster_jwt_tut/config/routes.rb
+++ thinkster_jwt_tut/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope :api, defaults: { format: :json } do
-    devise_for :users
+    devise_for :users, controllers: { sessions: :sessions },
+                       path_names: { sign_in: :login }
   end
 
 end
