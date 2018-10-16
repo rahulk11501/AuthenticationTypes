3e82af6af1e516705ee5be0d16abbaef14eff41a
rahul kumar <rahulkumar11501@gmail.com>
2018-10-16 00:57:47 -0700

    created own authenticate_user method

--- thinkster_jwt_tut/app/controllers/application_controller.rb
+++ thinkster_jwt_tut/app/controllers/application_controller.rb
@@ -1,9 +1,23 @@
 class ApplicationController < ActionController::API
-
+  
 
   private
-
+  
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.for(:sign_up) << :username
+  end
+
+  def authenticate_user
+    if request.headers['Authorization'].present?
+      authenticate_or_request_with_http_token do |token|
+        begin
+          jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
+
+          @current_user_id = jwt_payload['id']
+        rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
+          head :unauthorized
+        end
+      end
+    end
   end
 end
